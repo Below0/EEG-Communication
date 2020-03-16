72dfe973b96ce8ce53e5b775f73e9bb9df53d3ca
HAYOUNG LEE <l4538@naver.com>
2020-03-16 11:32:24 +0900

    delete unnecessary files

--- src/rasp/python-mindwave/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='mindwave',
-    version='0.2dev',
-    packages=['mindwave',],
-    license='BSD License',
-    long_description=open('README.txt').read(),
-)
